dc68a45248601d16076e81cc13e7630ed733da95
franciscomarin98 <francmarinc@gmail.com>
2022-01-03 14:47:17 -0500

    Lógica para mostrar los places buscados

--- src/app/maps/components/search-results/search-results.component.ts
+++ src/app/maps/components/search-results/search-results.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {MapService, PlacesService} from '../../services';
+import {Feature} from "../../interfaces/places";
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
   styleUrls: ['./search-results.component.css']
 })
-export class SearchResultsComponent implements OnInit {
+export class SearchResultsComponent {
 
-  constructor() { }
+  public selectedId: string = "";
 
-  ngOnInit(): void {
+  constructor(private placesService: PlacesService, private mapService: MapService) {
   }
 
+  get isLoadingPlaces(): boolean {
+    return this.placesService.isLoadingPlaces;
+  }
+
+  get places(): Feature[] {
+    return this.placesService.places;
+  }
+
+  flyTo(place: Feature) {
+    const [longitude, latitude] = place.center;
+    this.selectedId = place.id;
+    this.mapService.flyTo([longitude, latitude]);
+  }
 }
