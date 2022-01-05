721060d3689bbd4e18fc8943e4fb8386bf111c42
franciscomarin98 <francmarinc@gmail.com>
2022-01-04 22:27:29 -0500

    Trazando la PolyLyne

--- src/app/maps/services/map.service.ts
+++ src/app/maps/services/map.service.ts
@@ -1,95 +1,130 @@
 import {Injectable} from '@angular/core';
-import {LngLatBounds, LngLatLike, Map, Marker, Popup} from "mapbox-gl";
+import {AnySourceData, LngLatBounds, LngLatLike, Map, Marker, Popup} from "mapbox-gl";
 import {Feature} from "../interfaces/places";
 import {DirectionsAPIClient} from "../api/directionsAPIClient";
 import {DirectionResponse, Route} from "../interfaces/directions";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class MapService {
 
-  private map?: Map;
-  private markers: Marker[] = [];
+    private map?: Map;
+    private markers: Marker[] = [];
 
-  constructor(private directionsAPIClient: DirectionsAPIClient) {
-  }
+    constructor(private directionsAPIClient: DirectionsAPIClient) {
+    }
 
-  get isMapReady(): boolean {
-    return !!this.map;
-  }
+    get isMapReady(): boolean {
+        return !!this.map;
+    }
 
-  setMap(map: Map) {
-    this.map = map;
-  }
+    setMap(map: Map) {
+        this.map = map;
+    }
 
-  flyTo(coords: LngLatLike) {
-    if (!this.isMapReady) throw new Error("The map is not ready");
+    flyTo(coords: LngLatLike) {
+        if (!this.isMapReady) throw new Error("The map is not ready");
 
-    this.map?.flyTo({zoom: 14, center: coords});
-  }
+        this.map?.flyTo({zoom: 14, center: coords});
+    }
 
-  createMarkersFromPlaces(places: Feature[], userLocation: [number, number]) {
+    createMarkersFromPlaces(places: Feature[], userLocation: [number, number]) {
 
-    if (!this.map) throw Error('Mapa no inicializado');
+        if (!this.map) throw Error('Mapa no inicializado');
 
 
-    this.markers.forEach(marker => marker.remove());
-    const newMarkers = [];
+        this.markers.forEach(marker => marker.remove());
+        const newMarkers = [];
 
-    for (const place of places) {
-      const [lng, lat] = place.center;
-      const popup = new Popup()
-        .setHTML(`
+        for (const place of places) {
+            const [lng, lat] = place.center;
+            const popup = new Popup()
+                .setHTML(`
           <h6>${place.text}</h6>
           <span>${place.place_name}</span>
         `);
 
-      const newMarker = new Marker()
-        .setLngLat([lng, lat])
-        .setPopup(popup)
-        .addTo(this.map);
+            const newMarker = new Marker()
+                .setLngLat([lng, lat])
+                .setPopup(popup)
+                .addTo(this.map);
 
-      newMarkers.push(newMarker);
+            newMarkers.push(newMarker);
+        }
+
+        this.markers = newMarkers;
+
+        if (places.length === 0) return;
+
+        // Limites del mapa
+        const bounds = new LngLatBounds();
+        newMarkers.forEach(marker => bounds.extend(marker.getLngLat()));
+        bounds.extend(userLocation);
+
+        this.map.fitBounds(bounds, {
+            padding: 200
+        });
+
     }
 
-    this.markers = newMarkers;
+    getPointsBetweenToPoints(start: [number, number], end: [number, number]) {
+        this.directionsAPIClient.get<DirectionResponse>(`/mapbox/driving/${start.join(',')};${end.join(',')}`).subscribe(data => {
+            this.drawPolyline(data.routes[0])
+        })
+    }
 
-    if (places.length === 0) return;
+    drawPolyline(route: Route) {
+        console.log({
+            kms: route.distance / 1000,
+            duration: route.duration / 60,
+        });
 
-    // Limites del mapa
-    const bounds = new LngLatBounds();
-    newMarkers.forEach(marker => bounds.extend(marker.getLngLat()));
-    bounds.extend(userLocation);
+        if (!this.map) throw Error('Map is not initialized');
 
-    this.map.fitBounds(bounds, {
-      padding: 200
-    });
+        const coords = route.geometry.coordinates;
+        const bounds = new LngLatBounds();
+        coords.forEach(([lng, lat]) => {
+            bounds.extend([lng, lat])
+        });
 
-  }
+        this.map?.fitBounds(bounds, {
+            padding: 200
+        });
 
-  getPointsBetweenToPoints(start: [number, number], end: [number, number]) {
-    this.directionsAPIClient.get<DirectionResponse>(`/mapbox/driving/${start.join(',')};${end.join(',')}`).subscribe(data => {
-      this.drawPolyline(data.routes[0])
-    })
-  }
+        // Polyline
+        const sourceData: AnySourceData = {
+            type: 'geojson',
+            data: {
+                type: 'Feature',
+                properties: {},
+                geometry: {
+                    type: "LineString",
+                    coordinates: coords
+                }
+            }
+        }
 
-  drawPolyline(route: Route) {
-    console.log({
-      kms: route.distance / 1000,
-      duration: route.duration / 60,
-    });
+        if (this.map.getLayer('RouteString')) {
+            this.map.removeLayer('RouteString')
+            this.map.removeSource('RouteString')
+        }
 
-    if (!this.map) throw Error('Map is not initialized');
+        this.map.addSource('RouteString', sourceData);
+        this.map.addLayer({
+            id: 'RouteString',
+            type: 'line',
+            source: 'RouteString',
+            layout: {
+                "line-cap": 'round',
+                "line-join": 'round',
+            },
+            paint: {
+                "line-color": '#746DEF',
+                "line-width": 4
+            }
+        });
 
-    const coords = route.geometry.coordinates;
-    const bounds = new LngLatBounds();
-    coords.forEach(([lng, lat]) => {
-      bounds.extend([lng, lat])
-    });
 
-    this.map?.fitBounds(bounds, {
-      padding: 200
-    });
-  }
+    }
 }
